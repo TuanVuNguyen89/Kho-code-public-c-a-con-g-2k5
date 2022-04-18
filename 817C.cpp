305752b9e47f68db824045fae35ff7928d53b7a0
TuanVuNguyen89 <100076236+TuanVuNguyen89@users.noreply.github.com>
2022-04-18 17:32:12 +0700

    Add files via upload
    hmmm

---
+++ 817C.cpp
@@ -0,0 +1,42 @@
+/**
+*    Author :  Nguyen Tuan Vu
+*    Created : 18.04.2022
+**/
+
+#pragma GCC optimize("O2")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void file(){
+	#define TASK ""
+	freopen(TASK".inp","r",stdin);
+	freopen(TASK".out","w",stdout);
+}
+
+long long n, s;
+
+bool check(long long x) {
+    long long y = x;
+    while (y) {
+        x -= y%10;
+        y /= 10;
+    }
+    return (x >= s);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	//file();
+    cin >> n >> s;
+    long long l = s - 1, r = n + 1;
+    while (r - l > 1) {
+        long long mid = (l + r)>>1;
+        if (check(mid)) r = mid;
+        else l = mid;
+    }
+    cout << n - r + 1;
+	return 0;
+}
